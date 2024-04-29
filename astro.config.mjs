b9fa44f26c882d995b81e692f74f1dc7b19ac58a
jwcorle <jcorley.w@gmail.com>
2024-04-29 16:58:44 -0400

    feat: add work content collection and responsiveness

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import tailwind from "@astrojs/tailwind";
+
+import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: "https://corgo.tech",
+  integrations: [tailwind(), mdx()],
+  site: "https://corgo.tech"
 });
---
+++ src/content/config.ts
@@ -0,0 +1,14 @@
+import { z, defineCollection } from "astro:content";
+
+export const collections = {
+  work: defineCollection({
+    type: "content",
+    schema: ({ image }) =>
+      z.object({
+        title: z.string(),
+        client: z.string(),
+        tags: z.array(z.string()),
+        image: image(),
+      }),
+  }),
+};
